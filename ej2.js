91500d3a569e881190d2441cef32a78a8fa5421e
unknown <formacio@PC-1062629.formacio.local>
2022-05-09 11:36:29 +0200

    ejercicio2 parte 2

--- ej2.js
+++ ej2.js
@@ -1,5 +1,13 @@
+//EX1
 const name = 'Veronica';
 const firstSurname = 'Matarin';
 const secondSurname = 'Martinez';
 
 console.log(`My name is ${name}, my first surname is ${firstSurname} and my second surname is ${secondSurname} `)
+
+//EX2
+
+function years(age){
+    return age
+}
+console.log(`I am ${years(32)} years old`)
