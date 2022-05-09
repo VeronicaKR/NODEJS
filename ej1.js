d30f080a920430e32b29f1bf8f291e007db37286
unknown <formacio@PC-1062629.formacio.local>
2022-05-09 12:35:33 +0200

    ej3

--- ej1.js
+++ ej1.js
@@ -1,5 +1,32 @@
+//EX1
+/*
 function hello(name){
 console.log(`Hola ${name}`)
 }
 
 hello('Veronica')
+
+// EX2.1
+const name = 'Veronica';
+const firstSurname = 'Matarin';
+const secondSurname = 'Martinez';
+
+console.log(`My name is ${name}, my first surname is ${firstSurname} and my second surname is ${secondSurname} `)
+
+//EX2.2
+function years(age){
+    return age
+}
+console.log(`I am ${years(32)} years old`)
+*/
+//EX3
+let array = []
+    
+function calculate(){
+     for(let i=0; i<10; i++){
+    console.log(i)}} 
+
+for (let i=0; i<10; i++){
+    array.push(calculate())
+}
+console.log(array)
