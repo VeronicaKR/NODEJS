6b37219f9cfcec44137baf74ed761b3e01515313
unknown <formacio@PC-1062629.formacio.local>
2022-05-09 11:32:02 +0200

    ejercicio 2 parte 1

---
+++ ej2.js
@@ -0,0 +1,5 @@
+const name = 'Veronica';
+const firstSurname = 'Matarin';
+const secondSurname = 'Martinez';
+
+console.log(`My name is ${name}, my first surname is ${firstSurname} and my second surname is ${secondSurname} `)
