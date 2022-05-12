8393b2a57f33c5742358ab1fe98520226c3a39fd
Veronica <veronicamatarinmartinez@gmail.com>
2022-05-12 11:41:01 +0200

    ex1y2

--- entrega1.3PROMISESCALLBACKS.js
+++ entrega1.3PROMISESCALLBACKS.js
@@ -1,20 +1,61 @@
 //LEVEL 1 EXERCICE 1
-
-function devolverPromesa(result){
-    return new Promise(function(resolve,reject){
-        
-        if (result === 1){
-            resolve ('TRUE')
-        }else {
-            reject('ERROR');
+const frutas = [{
+    fruit: 'manzana',
+    color: 'verde',
+    },{
+        fruit:'fresa',
+        color:'rojo'
+    }]
+    
+function getDatos (){
+    return new Promise ((resolve,reject)=> {
+        if (frutas.length>1){
+            resolve ('MAYOR QUE UNO')
+        }else if (frutas.length<1){
+            reject('MENOR DE UNO');
             }
-    })
+    });
 }
 
-devolverPromesa().then(function(message){
+const ok = getDatos()
+
+ok.then(function(message){
     console.log(message);
 }).catch(function(error){
     console.log(error);
 });
 
-devolverPromesa(1)
+function getDatosInc (){
+    return new Promise ((resolve,reject) =>{
+    if (frutas[0].color==='rojo'){
+        resolve ('COLOR ROJO')
+    }else if (frutas[0].color==='verde'){
+        reject('COLOR VERDE');
+        }
+    });
+}
+const noOk = getDatosInc()
+noOk.then(function(message){
+    console.log(message);
+}).catch(function(error){
+    console.log(error);
+});
+
+//LEVEL 1 EXERCICE 2
+
+function callback(param){
+    console.log(param)
+}
+
+let isEven = (element,callback) => {
+    if (element %2 === 0){
+        callback('EVEN')
+    } else {
+        callback('ODD')}
+}
+
+isEven(2,callback)
+isEven(3,callback)
+
+//LEVEL 2 EXERCICE 1
+
