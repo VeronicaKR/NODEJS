8f625a84ca9048790476f10a813c86d34e388cd3
Veronica <veronicamatarinmartinez@gmail.com>
2022-05-19 13:19:06 +0200

    ejercicio5

--- entrega1.5NODEUTILS.js
+++ entrega1.5NODEUTILS.js
@@ -1,49 +1,42 @@
-/* //LEVEL 1 EXERCICES
-
+//LEVEL 1 EXERCICES
 var fs = require('fs');
-var archiver = require('archiver')
-var output = fs.createWriteStream('./'+ Date.now()+'.zip');
-var archive = archiver('zip')
-archive.pipe(output); 
-
+//var archiver = require('archiver')
+//var output = fs.createWriteStream('./'+ Date.now()+'.zip');
+//var archive = archiver('zip')
+//archive.pipe(output); 
+/*
 //PHRASE
-fs.writeFile('nuevo.txt', 'Hello world\nGoodbye\n', function(err){
-    if (err){
+fs.writeFile('nuevo.txt', 'Hello world\nGoodbye\n', function (err) {
+    if (err) {
         return console.log(err);
     }
     console.log('archivo creado')
 });
+
 //MESSAGE
 fs.readFile('nuevo.txt', 'utf8', function(err,data){
     if(err){
         return console.log(err)
-    }
-    console.log(data)
-})
+    }console.log(data)
+}) 
 
 //COMPRESSED
 archive.append(fs.createReadStream('./nuevo.txt'),{name:'nuevoZip.txt'});
-
 archive.directory('./VNode', 'archivo')
-
 archive.finalize();
 
 //LEVEL2 EXERCICE 1
-
 function sayHello() {console.log('Hola')}
 let hello = setInterval(sayHello,1000);
 
 setTimeout(function(){
     clearInterval(hello)},8000
 ) 
-*/
-
 //LEVEL 2 EXERCICE 2
-
 const {spawn} = require('child_process');
 const { stdout, stderr } = require('process');
 const ls = spawn('dir', [], {shell:true});
-ls.stdout.on('data', data=> {
+ls.stdout.on('data', data=> {docu
     console.log(`stodout: ${data}`);
 })
 ls.stderr.on('data', data => {
@@ -54,38 +47,100 @@ ls.on('error', (error) => {
 })
 ls.on('close', code =>{
     console.log(`${code}`)
-})   
+})   */
 
 // LEVEL 3 EXERCICE 1
-//comentar todo el ejercicio e ir descomentando ejercicio a ejercicio
-/* function createTwoFiles(){
-var fs = require('fs');
-fs.writeFile('nuevo.txt', 'Hello world', function(err){
-    if (err){
-        return console.log(err);
+function ex1() {
+    //codificar y crear el archivo inicial a hexadecimal
+    fs.readFile('nuevo.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err)
+        } else {
+            let text = ''
+            for (i = 0; i < data.length; i++) {
+                //console.log(data+'1')
+                text += data.charCodeAt(i).toString(16)
+            } console.log(text)
+            fs.writeFile('text-hexadecimal.txt', text, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.writeFile('text-hexadecimal.txt', text, function (err) {
+                if (err) {
+                    return console.log(err);
+                }console.log('archivo creado')
+            });
+        }
+    })
+
+
+//codificar y crear el archivo inicial a base64
+fs.readFile('nuevo.txt', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err)
+    } else {
+        let buff = new Buffer.from(data);
+        let texto2 = buff.toString('base64')
+
+        fs.writeFile('texto2-base64.txt', texto2, function (err) {
+            if (err) {
+                return console.log(err);
+            }console.log('archivo creado')
+        });console.log(texto2)
     }
-    console.log('archivo creado')
-});
-const path = './nuevo.txt'
-
-let text
-
-try {
-    if (fs.existsSync(path)){
-        console.log('existe')
-        fs.readFile('nuevo.txt', 'utf8', function(err,data){
-            if(err){
-                return console.log(err)
-            }
-           console.log(data)
-           text = data.split('').reduce((a, b)=> a.concat(Number(b.charCodeAt(0).toString(16)), ''))
-           console.log(text)
-            })
-  }
-}catch(error){
-    console.log(error)
+})
 }
-}
+//ex1()
 
-createTwoFiles() */
-0
+//encriptar el archivo codificado en hexadecimal 
+function ex2() {
+    fs.readFile('text-hexadecimal.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err)
+        } else {
+            let CryptoJS = require('crypto')
+            let key = 'R2d2'
+            let iv = 'R2d2'
+            key = CryptoJS.enc.Utf8.parse(key);
+            iv = CryptoJS.enc.Utf8.parse(iv);
+            let text3 = CryptoJS.AES.encrypt(data, key, {
+                iv: iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            });
+            text3 = text3.toString()
+            console.log(text3)
+            fs.writeFile('text-hexadecimal.txt', text3, function (err) {
+                if (err) {
+                    return console.log(err);
+                }console.log('archivo creado hexadecimal')
+            });
+        }
+    })
+//encriptar el archivo codificado a base 64 
+    fs.readFile('texto2-base64.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err)
+        } else {
+            let CryptoJS = require('crypto')
+            let key = 'R2d2'
+            let iv = 'R2d2'
+            key = CryptoJS.enc.Utf8.parse(key);
+            iv = CryptoJS.enc.Utf8.parse(iv);
+            let text3 = CryptoJS.AES.encrypt(data, key, {
+                iv: iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            });
+            text3 = text3.toString()
+            console.log(text3)
+            fs.writeFile('texto2-base64.txt', text3, function (err) {
+                if (err) {
+                    return console.log(err);
+                }console.log('archivo creado base64')
+            });
+        }
+    })
+}
+ex2()
