cffb7fa6ac620dc634d7fa67f47142e16f8e8895
VeronicaKR <veronicamatarinmartinez@gmail.com>
2022-05-24 11:23:06 +0200

    ejercicio 6

--- ej6/_test_/app.test.js
+++ ej6/_test_/app.test.js
@@ -1,32 +1,49 @@
-const Calculator = require('../app/app.js');
-const {it, expect} = require("@jest/globals");
-const {employees, salaries,getEmployeSalary} = require('C:/Users/formacio/Desktop/VNode/Sprint1-1/entrega1.4ASYNCAWAIT.js')
+const {sum,substrack,multiply,divide} = require('../app/app.js');
 
-describe('TESTING DE CALCULATOR',()=>{
-    const calculator = new Calculator;
-    it('sumar 1+1+1 da 3', ()=> {
-        const resultado = calculator.sum(1,1,1)
-    expect(resultado).toBe(3);
-});
-    it ('restar 4 - 2 - 1 da 1', ()=>{
-        const resultado = calculator.rest(4,2,1)
-    expect(resultado).toBe(1);
-});
-    it ('multiplicar 2 * 2 * 2 da 8', ()=>{
-        const resultado = calculator.multiply(2,2,2)
-        expect(resultado).toBe(8)
-    })
-    it ('dividir 10 entre 2 entre 2 da 2.5', ()=>{
-        const resultado = calculator.split(10,2,2)
-        expect(resultado).toBe(2.5)
-    })
-})
+const {getEmployeSalary, multiply2, total} = require('../../entrega1.4ASYNCAWAIT')
 
-describe('TESTING DE GETEMPLOYESALARY', ()=>{
-  test ('retorna un nombre y un salario de un empleado', ()=>{
-      expect(getEmployeSalary(employees.name,salaries.salary)).toBe(console.log('Bill Gates', 1000))
+test('sumar 1+1+1 da 3', ()=> {
+    const resultado = sum(1,1,1)
+expect(resultado).toBe(3);
+});
+test ('restar 4 - 2 - 1 da 1', ()=>{
+    const resultado = substrack(4,2,1)
+expect(resultado).toBe(1);
+});
+test ('multiplicar 2 * 2 * 2 da 8', ()=>{
+    const resultado = multiply(2,2,2)
+expect(resultado).toBe(8)
+    })
+test ('dividir 10 entre 2 entre 2 da 2.5', ()=>{
+    const resultado = divide(10,2,2)
+expect(resultado).toBe(2.5)
+    })
+
+//ASYNC AWAIT LEVEL 1 EX 1
+  test ('retorna un nombre y un salario de un empleado', async ()=>{
+    const result = await getEmployeSalary(2);  
+    expect(result).toEqual('el empleado Bill Gates gana 1000')
   })
-})
+
+  test ('retorna un nombre y un salario de un empleado', async ()=>{
+    const result = await getEmployeSalary(4);  
+    expect(result).toBe('el empleado Bill Gates gana 1000')
+  })
+
+//ASYNC AWAIT LEVEL 2 EX 1
+test ('retorna el doble del numero informado despues de dos segundos', async ()=>{
+    jest.setTimeout(2000); () => {
+        const result = multiply2(4);  
+        expect(result).toBe(16) 
+    }
+    })
+
+test ('retorna el doble del numero informado despues de dos segundos', async ()=>{
+    jest.setTimeout(2000); () => {
+        const result = total(1,2,3);  
+        expect(result).toBe(12) 
+    }
+    })
 
 
 
--- ej6/app/app.js
+++ ej6/app/app.js
@@ -1,29 +1,24 @@
 //FUNCIONES CALCULADORA
-class Calculator{
-
-sum (a,b,c){
-    let sum = a + b + c
-    return sum
+function sum (a,b,c){
+    let suma = a + b + c
+    return suma
 }
 
-
-rest(a, b,c){
+function substrack(a, b,c){
     let rest = a-b-c
     return rest
 }
 
-multiply(a,b,c){
+function multiply(a,b,c){
     let multiply = a*b*c
     return multiply
 }
-split(a,b,c){
+function divide(a,b,c){
     let split = a/b/c
     return split
 }
-}
 
-module.exports = Calculator;
-
+module.exports = {sum, substrack,multiply,divide}
 
 
 
